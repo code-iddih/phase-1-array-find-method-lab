6323b61f5258485bfb64d2feefec56c42b9ec322
code-iddih <idinyamwaya@gmail.com>
2024-07-01 23:04:21 +0300

    Completing the Tasks

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // code your solution here
+
+// Case 1 - When there is a WIN in the Record
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    // more years and results...
+  ];
+  
+  function superbowlWin(records) {
+    const winningRecord = records.find(record => record.result === "W");
+    return winningRecord ? winningRecord.year : undefined;
+  }
+  
+  // Example
+  console.log(superbowlWin(record)); // Running the example will give an Output: "2015"
+
+  // Case 2 - When there is NO WIN in the Record
+
+//   const record = [
+//     { year: "2015", result: "L"},
+//     { year: "2014", result: "N/A"},
+//     { year: "2013", result: "L"},
+//     // more years and results...
+//   ];
+  
+//   function superbowlWin(records) {
+//     const winningRecord = records.find(record => record.result === "W");
+//     return winningRecord ? winningRecord.year : undefined;
+//   }
+  
+//   // Example
+//   console.log(superbowlWin(record)); // Running the example will give an Output: "Undefined"
+
+
+
+// NB: if you want to check for Case 2, comment case 1 and Uncomment Case 2.
+
+
+
+  
